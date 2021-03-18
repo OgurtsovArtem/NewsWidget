1226949a6a1df2a3636e67b67cab95e02e9e7980
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2021-03-18 18:01:19 +0700

    добавил блок catch

--- src/js/api/Api.js
+++ src/js/api/Api.js
@@ -5,7 +5,12 @@ export default class Api {
   }
 
   async getNews() {
-    const res = await fetch('http://newsapi.org/v2/everything?domains=wsj.com&apiKey=cefe7cbdc6b94355ae9ee08e1066ea96');
-    return res.json();
+    try {
+      const res = await fetch('http://newsapi.org/v2/everything?domains=wsj.com&apiKey=cefe7cbdc6b94355ae9ee08e1066ea96');
+      return res.json();
+    } catch (err) {
+      console.log(`Ошибка ${err}`);
+      return err;
+    }
   }
 }
