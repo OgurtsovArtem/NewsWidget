cd2bbfc825608cfa181689eb8e9df350bc39a4fa
Artem Ogurtsov <ogurcov.artem2012@list.ru>
2021-03-19 13:47:35 +0700

    Изменил адррес на запрос через прокси-сервер

--- src/js/api/Api.js
+++ src/js/api/Api.js
@@ -6,7 +6,7 @@ export default class Api {
 
   async getNews() {
     try {
-      const res = await fetch('http://newsapi.org/v2/everything?domains=wsj.com&apiKey=cefe7cbdc6b94355ae9ee08e1066ea96');
+      const res = await fetch('https://nomoreparties.co/news/v2/top-headlines?country=us&apiKey=cefe7cbdc6b94355ae9ee08e1066ea96');
       return res.json();
     } catch (err) {
       console.log(`Ошибка ${err}`);
